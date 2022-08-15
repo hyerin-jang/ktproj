811d3cc9f53a76a00e787e281f3da06d0660f682
hyerin-jang <hrjang92@gmail.com>
2022-08-15 23:11:25 +0900

    init commit

---
+++ src/main/kotlin/com/project/ktproj/KtprojApplication.kt
@@ -0,0 +1,11 @@
+package com.project.ktproj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtprojApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtprojApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/ktproj/KtprojApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.ktproj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtprojApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
